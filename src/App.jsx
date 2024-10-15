078640554e1e457d95feb8203d6cab31a7243f0b
Shah Zaib <144054775+ShahZaib151214@users.noreply.github.com>
2024-10-15 10:17:36 +0500

    first project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './Pages/HomePage/HomePage';
 import AboutUs from './Pages/AboutUs/AboutUs';
--- src/Appcontent.jsx
+++ src/Appcontent.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import {Routes, Route, useLocation } from "react-router-dom";
 import HomePage from './Pages/HomePage/HomePage';
 import AboutUs from './Pages/AboutUs/AboutUs';
 import Login from './Component/Login/Login';
